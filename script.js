f33eec20da2eee8ffa1f186b93f012cc4e0e4dc6
Matthew Creek <matthew.creek27@gmail.com>
2024-04-28 22:41:04 -0500

    cleaned up console logs

--- script.js
+++ script.js
@@ -29,12 +29,12 @@ window.addEventListener("load", function() {
     let listedPlanetsResponse = myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
-        console.log(listedPlanets);
+        // console.log(listedPlanets);
     }).then(function () {
-        console.log(listedPlanets);
+        // console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
         let selectedPlanet = pickPlanet(listedPlanets)
-        console.log(selectedPlanet)
+        // console.log(selectedPlanet)
         addDestinationInfo(document, selectedPlanet.name, selectedPlanet.diameter, selectedPlanet.star, selectedPlanet.distance, selectedPlanet.moons, selectedPlanet.image)
     })
 
