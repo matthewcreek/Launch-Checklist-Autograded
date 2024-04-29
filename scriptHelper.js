f4687c7251127719db6708f58941575accf83cc1
Matthew Creek <matthew.creek27@gmail.com>
2024-04-28 22:39:49 -0500

    all tests passing

--- scriptHelper.js
+++ scriptHelper.js
@@ -3,18 +3,21 @@
 require('cross-fetch/polyfill');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
+    let missionTarget = document.getElementById('missionTarget')
     // Here is the HTML formatting for our mission target div.
-    /*
-                 <h2>Mission Destination</h2>
-                 <ol>
-                     <li>Name: </li>
-                     <li>Diameter: </li>
-                     <li>Star: ${star}</li>
-                     <li>Distance from Earth: </li>
-                     <li>Number of Moons: </li>
-                 </ol>
-                 <img src="">
-    */
+    missionTarget.innerHTML = `
+        <h2>Mission Destination</h2>
+        <ol>
+        <li>Name: ${name}</li>
+        <li>Diameter: ${diameter}</li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance}</li>
+        <li>Number of Moons: ${moons}</li>
+        </ol>
+        <img src=${imageUrl}>
+    `
+    
+
  }
  
  function validateInput(testInput) {
@@ -82,13 +85,16 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  async function myFetch() {
      let planetsReturned;
  
-     planetsReturned = await fetch().then( function(response) {
-         });
+     planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json').then( function(response) {
+        return response.json()
+        });
  
      return planetsReturned;
  }
  
  function pickPlanet(planets) {
+    let selection = Math.floor(Math.random() * planets.length)
+    return planets[selection]
  }
  
  module.exports.addDestinationInfo = addDestinationInfo;
